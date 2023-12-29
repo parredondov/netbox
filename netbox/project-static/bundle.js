4cc081bc13b26b1d627112662acb5ac5546fd570
Jeremy Stretch <jstretch@netboxlabs.com>
2023-12-29 12:29:22 -0500

    Remove dark mode styling

--- netbox/project-static/bundle.js
+++ netbox/project-static/bundle.js
@@ -75,7 +75,7 @@ async function bundleStyles() {
     const entryPoints = {
       'netbox-external': 'styles/_external.scss',
       'netbox-light': 'styles/_light.scss',
-      'netbox-dark': 'styles/_dark.scss',
+      // 'netbox-dark': 'styles/_dark.scss',
       'netbox-print': 'styles/_print.scss',
       rack_elevation: 'styles/_rack_elevation.scss',
       cable_trace: 'styles/_cable_trace.scss',
